9d4e7d54e141bf7b1a805ac25857bbf2bbf3bba2
yankikucuk <kendinikertenkelebek@me.com>
2022-09-19 14:07:59 -0700

    Init

---
+++ init.lua
@@ -0,0 +1,51 @@
+local LSM = LibStub("LibSharedMedia-3.0")
+
+if LSM == nil then return end
+
+-- Background
+
+-- Border
+
+-- Fonts
+LSM:Register("font", "ITC-Avant-Garde-Gothic", [[Interface\Addons\LMT\Fonts\ITC-Avant-Garde-Gothic.ttf]])
+LSM:Register("font", "Montserrat-Medium", [[Interface\Addons\LMT\Fonts\Montserrat-Medium.ttf]])
+LSM:Register("font", "Montserrat-Regular", [[Interface\Addons\LMT\Fonts\Montserrat-Regular.ttf]])
+LSM:Register("font", "Montserrat-SemiBold", [[Interface\Addons\LMT\Fonts\Montserrat-SemiBold.ttf]])
+LSM:Register("font", "Play-Bold", [[Interface\Addons\LMT\Fonts\Play-Bold.ttf]])
+LSM:Register("font", "Play-Regular", [[Interface\Addons\LMT\Fonts\Play-Regular.ttf]])
+LSM:Register("font", "Poppins-Medium", [[Interface\Addons\LMT\Fonts\Poppins-Medium.ttf]])
+LSM:Register("font", "Poppins-Regular", [[Interface\Addons\LMT\Fonts\Poppins-Regular.ttf]])
+LSM:Register("font", "Poppins-SemiBold", [[Interface\Addons\LMT\Fonts\Poppins-SemiBold.ttf]])
+
+-- Sounds
+LSM:Register("sound", "Clearcasting", [[Interface\Addons\LMT\Sounds\Clearcasting.ogg]])
+LSM:Register("sound", "Coin-Alternate", [[Interface\Addons\LMT\Sounds\Coin-Alternate.ogg]])
+LSM:Register("sound", "Coin-Alternate2", [[Interface\Addons\LMT\Sounds\Coin-Alternate2.ogg]])
+LSM:Register("sound", "Coin-Big", [[Interface\Addons\LMT\Sounds\Coin-Big.ogg]])
+LSM:Register("sound", "Coin-Normal", [[Interface\Addons\LMT\Sounds\Coin-Normal.ogg]])
+LSM:Register("sound", "Coin-SemiNormal", [[Interface\Addons\LMT\Sounds\Coin-SemiNormal.ogg]])
+LSM:Register("sound", "Coin-Sweet", [[Interface\Addons\LMT\Sounds\Coin-Sweet.ogg]])
+LSM:Register("sound", "CR", [[Interface\Addons\LMT\Sounds\CR.ogg]])
+LSM:Register("sound", "Dead", [[Interface\Addons\LMT\Sounds\Dead.ogg]])
+
+-- Statusbars
+LSM:Register("statusbar", "Atrocity", [[Interface\Addons\LMT\Statusbars\Atrocity.tga]])
+LSM:Register("statusbar", "ElvUI A", [[Interface\Addons\LMT\Statusbars\ElvUI A.tga]])
+LSM:Register("statusbar", "ElvUI B", [[Interface\Addons\LMT\Statusbars\ElvUI B.tga]])
+LSM:Register("statusbar", "ElvUI C", [[Interface\Addons\LMT\Statusbars\ElvUI C.tga]])
+LSM:Register("statusbar", "ElvUI D", [[Interface\Addons\LMT\Statusbars\ElvUI D.tga]])
+LSM:Register("statusbar", "ElvUI E", [[Interface\Addons\LMT\Statusbars\ElvUI E.tga]])
+LSM:Register("statusbar", "ElvUI F", [[Interface\Addons\LMT\Statusbars\ElvUI F.tga]])
+LSM:Register("statusbar", "ElvUI G", [[Interface\Addons\LMT\Statusbars\ElvUI G.tga]])
+LSM:Register("statusbar", "ElvUI H", [[Interface\Addons\LMT\Statusbars\ElvUI H.tga]])
+LSM:Register("statusbar", "ElvUI I", [[Interface\Addons\LMT\Statusbars\ElvUI I.tga]])
+LSM:Register("statusbar", "ElvUI J", [[Interface\Addons\LMT\Statusbars\ElvUI J.tga]])
+LSM:Register("statusbar", "ElvUI K", [[Interface\Addons\LMT\Statusbars\ElvUI K.tga]])
+LSM:Register("statusbar", "ElvUI L", [[Interface\Addons\LMT\Statusbars\ElvUI L.tga]])
+LSM:Register("statusbar", "ElvUI M", [[Interface\Addons\LMT\Statusbars\ElvUI M.tga]])
+LSM:Register("statusbar", "ElvUI N", [[Interface\Addons\LMT\Statusbars\ElvUI N.tga]])
+LSM:Register("statusbar", "ElvUI O", [[Interface\Addons\LMT\Statusbars\ElvUI O.tga]])
+LSM:Register("statusbar", "ElvUI P", [[Interface\Addons\LMT\Statusbars\ElvUI P.tga]])
+LSM:Register("statusbar", "ElvUI Q", [[Interface\Addons\LMT\Statusbars\ElvUI Q.tga]])
+LSM:Register("statusbar", "ElvUI R", [[Interface\Addons\LMT\Statusbars\ElvUI R.tga]])
+LSM:Register("statusbar", "Skullflower", [[Interface\Addons\LMT\Statusbars\Skullflower.tga]])
